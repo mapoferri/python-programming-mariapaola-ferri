f024512e800e49c064342c97160f986cf05de178
mapoferri <45259338+mapoferri@users.noreply.github.com>
2018-11-27 14:51:03 +0100

    Update volume.py

--- volume.py
+++ volume.py
@@ -1,5 +1,6 @@
-from math import pi
-diam = 10**(-9)
-v= (4*pi*((diam/2)**(3)))
-print(v)
+#Computing the volume of the sphere
+from math import pi       #importing phi from the math library
+diam = 10**(-9)           #considering the diameter as 10 picom    
+volume = (4*pi*((diam/2)**(3)))
+print (volume)
 
